230cf82016186482e23e822bc7290673f17a4118
Tom MacWright <tom@macwright.org>
2014-10-07 19:47:51 -0400

    Eval pause

--- static/index.js
+++ static/index.js
@@ -11,19 +11,22 @@ insertCss(fs.readFileSync(__dirname + '/css/site.css', 'utf8'));
 
 var stream = shoe('/eval');
 var error = document.getElementById('error');
+var evalIndicator = document.getElementById('eval-indicator');
 var widgets = [];
+var evalPause = false;
+var delayedClear;
 
 function makeWidget(name, x) {
   var indent = false;
-  var msg = document.createElement("div");
+  var msg = document.createElement('div');
   msg.className = 'data';
-  var pre = msg.appendChild(document.createElement("pre"));
+  var pre = msg.appendChild(document.createElement('pre'));
   pre.appendChild(document.createTextNode(x));
   pre.onclick = function() {
     indent = !indent;
     pre.innerHTML = JSON.stringify(JSON.parse(x), null, indent ? 2 : null);
   };
-  var n = msg.appendChild(document.createElement("div"));
+  var n = msg.appendChild(document.createElement('div'));
   n.className = 'data-name';
   n.innerHTML = name;
   return msg;
@@ -35,8 +38,6 @@ function clearData() {
   });
   widgets = [];
 }
-
-var delayedClear;
 
 function read(str) {
   var d = JSON.parse(str);
@@ -66,6 +67,20 @@ function read(str) {
   }
 }
 
+function togglePauseEval() {
+  evalPause = !evalPause;
+  if (evalPause) {
+    evalIndicator.innerHTML = 'paused';
+    evalIndicator.className = '';
+  } else {
+    evalIndicator.innerHTML = 'live';
+    evalIndicator.className = 'quiet';
+  }
+  return false;
+}
+
+evalIndicator.onclick = togglePauseEval;
+
 function values(d) {
   return Object.keys(d).map(function(k) { return d[k]; });
 }
@@ -74,12 +89,17 @@ var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
   indentUnit: 2,
   mode: 'text/javascript',
   lineNumbers: true,
-  autofocus: true
+  autofocus: true,
+  extraKeys: {
+    'Ctrl-E': togglePauseEval,
+    'Cmd-E': togglePauseEval
+  }
 });
 
 editor.setOption('theme', 'monokai');
 
 editor.on('change', function() {
+  if (evalPause) return;
   clearTimeout(delayedClear);
   stream.write(editor.getValue());
 });
